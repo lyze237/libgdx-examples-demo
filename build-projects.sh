9d38730a4e606517056ab6576c02a75a5537917d
Lyze <11274700+lyze237@users.noreply.github.com>
2022-01-19 23:06:11 +0100

    init

---
+++ build-projects.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+OUTPUT_DIR="ghpages"
+mkdir -p "$OUTPUT_DIR"
+
+while IFS= read -r -d '' -u 9
+do 
+    # only find repos which contain a html module
+    if [[ "$REPLY" == */html/build.gradle ]]; then
+        # Remove html folder and gradle filename so we figure out the projects folder
+        FOLDER="${REPLY%"/html/build.gradle"}"
+        # Get the actual gradle file
+        GRADLE_FILE="$FOLDER/build.gradle"
+
+        if test -f "$GRADLE_FILE"; then
+            echo "Building $GRADLE_FILE"
+            (cd "$FOLDER" && pwd ; chmod u+x gradlew && ./gradlew html:dist)
+
+            if [[ $? -eq 0 ]]; then
+                DIST_FOLDER="$FOLDER/html/build/dist/."
+                OUTPUT_FOLDER="$OUTPUT_DIR/$FOLDER"
+                echo "Copying $DIST_FOLDER to $OUTPUT_FOLDER"
+                mkdir -p "$OUTPUT_FOLDER"
+                cp -r "$DIST_FOLDER" "$OUTPUT_FOLDER"
+            else
+                echo "Couldn't build $GRADLE_FILE"
+                exit 2
+            fi
+        else
+            echo "$GRADLE_FILE doesnt exist"
+            exit 1
+        fi
+    fi
+done 9< <( find . -type f -name "build.gradle" -exec printf '%s\0' {} + )
